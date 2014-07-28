765f3bdf9e1c3baf1bbfe0bedfbb0de6af24b2c3
ulgu <ulf.gundersen@gmail.com>
2014-07-28 15:33:55 +0200

    First version

---
+++ src/Calculate.py
@@ -0,0 +1,19 @@
+import math
+
+__author__ = 'ulfgun'
+
+class Calculate:
+    def Demo (self):
+            while True:
+                a = int (input("a "))
+                b = int (input("b "))
+                c = int (input("c "))
+                d = b**2-4*a*c
+                if d>=0:
+                    disc = math.sqrt(d)
+                    root1 =(-b+disc) / (2*a)
+                    root2 = (-b - disc) / (2*a)
+                    print (root1,root2)
+                else:
+                    print ("error")
+Calculate().Demo()
